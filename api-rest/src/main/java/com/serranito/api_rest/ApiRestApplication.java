5ffa3805d9e1df2f436626231a91f1e582480aef
Hernan Acero <contacto.acero.hernan@gmail.com>
2026-05-23 10:12:41 -0500

    Se cambio configuración de docker

--- api-rest/src/main/java/com/serranito/api_rest/ApiRestApplication.java
+++ api-rest/src/main/java/com/serranito/api_rest/ApiRestApplication.java
@@ -2,10 +2,17 @@ package com.serranito.api_rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
+import org.springframework.context.ApplicationListener;
 
 @SpringBootApplication
 public class ApiRestApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(ApiRestApplication.class, args);
+		SpringApplication app = new SpringApplication(ApiRestApplication.class);
+		app.addListeners((ApplicationListener<ApplicationEnvironmentPreparedEvent>) event -> {
+			String url = event.getEnvironment().getProperty("spring.datasource.url");
+			System.out.println("spring.datasource.url=" + url);
+		});
+		app.run(args);
 	}
 }
